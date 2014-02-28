f6a61acdd5826407d8a71517f635aed42118e0b9
lsck_74110@qq.com <lsck_74110@qq.com>
2014-03-01 01:40:32 +0800

    笨方法作业
    更新ex9

--- ex34.py
+++
@@ -1,7 +0,0 @@
-animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
-
-for i in range(len(animals)):
-    print  'the %dst animal is at %d and is a %s.' %(i+1,i,animals[i])
-    print  'The animal at %d is the %dst animal and is a %s.' %(i,i+1,animals[i])
-    print
-    print
